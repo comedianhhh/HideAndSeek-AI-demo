45944db3eae6621a8d1b1b2702791681048f90fb
Nianzhi Li <100120646+comedianhhh@users.noreply.github.com>
2024-03-05 15:32:04 -0500

    done

--- Assets/Game/Scripts/Y-Bot/YBot_CoverState.cs
+++ Assets/Game/Scripts/Y-Bot/YBot_CoverState.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static YBot_CoverState;
 using static YBot_JumpWithStyleState;
 
 public class YBot_CoverState : YBot_BaseState
@@ -12,8 +13,8 @@ public class YBot_CoverState : YBot_BaseState
         CoverToStand
     }
     public CoverStates coverStates = CoverStates.StandToCover;
-    
 
+    public bool HasBeenCaught=false;
 
     public float distanceToWall=0.3f;
 
@@ -38,8 +39,10 @@ public class YBot_CoverState : YBot_BaseState
         animationListener.AddAnimationCompletedListener(ANIM_StandToCover, OnAnimationCompleted);
         animationListener.AddAnimationCompletedListener(ANIM_CoverToStand, OnAnimationCompleted);
 
-        coverStates = CoverStates.CoverToStand;
+        GameManager.Instance.OnFound += OnFoundHandler;
 
+        coverStates = CoverStates.StandToCover;
+        HasBeenCaught = false;
         AlignWithCover();
 
         YBotAnimator.SetTrigger("Cover");
@@ -47,15 +50,34 @@ public class YBot_CoverState : YBot_BaseState
     }
 
 
+
+    public override void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        //switch (coverStates)
+        //{
+        //    case CoverStates.StandToCover:
+        //        break;
+        //    case CoverStates.Cover:
+        //        break;
+        //    case CoverStates.CoverToStand:
+        //        break;
+
+        //}
+    }
+
+    public override void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        GameManager.Instance.OnFound-= OnFoundHandler;
+    }
     private void OnAnimationCompleted(int shortHashCode)
     {
         if (shortHashCode == ANIM_StandToCover)
         {
-            coverStates = CoverStates.StandToCover;
+            coverStates = CoverStates.Cover;
         }
         else if (shortHashCode == ANIM_CoverToStand)
         {
-            coverStates = CoverStates.CoverToStand;
+            fsm.ChangeState(fsm.CryStateName);
         }
     }
 
@@ -98,6 +120,17 @@ public class YBot_CoverState : YBot_BaseState
         }
 
         return hasHit;
+    }
+
+    void OnFoundHandler()
+    {
+        HasBeenCaught = true;
+        if (coverStates == CoverStates.Cover)
+        {
+            YBotAnimator.SetTrigger("FoundCover");
+        }
+        coverStates = CoverStates.CoverToStand;
+        Debug.Log("I've been found!");
     }
     private void OnAnimationMove()
     {
--- Assets/Game/Scripts/Y-Bot/YBot_CryState.cs
+++ Assets/Game/Scripts/Y-Bot/YBot_CryState.cs
@@ -5,4 +5,36 @@ using UnityEngine;
 public class YBot_CryState : YBot_BaseState
 {
 
+    private AnimationListener animationListener;
+
+    private readonly int ANIM_Crying = Animator.StringToHash("Crying");
+
+
+    public override void Init(GameObject _owner, FSM _fsm)
+    {
+        base.Init(_owner, _fsm);
+
+        animationListener = owner.GetComponent<AnimationListener>();
+
+        Debug.Assert(animationListener != null, $"{owner.name}AnimationListener not found");
+    }
+    public override void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        base.OnStateEnter(animator, stateInfo, layerIndex);
+        animationListener.AddAnimationCompletedListener(ANIM_Crying, OnAnimationCompleted);
+        agent.enabled = false;
+    }
+
+    public override void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        agent.enabled = true;
+    }
+
+    private void OnAnimationCompleted(int shortHashCode)
+    {
+        if(shortHashCode== ANIM_Crying)
+        {
+            fsm.ChangeState(fsm.JogStateName);
+        }
+    }
 }
